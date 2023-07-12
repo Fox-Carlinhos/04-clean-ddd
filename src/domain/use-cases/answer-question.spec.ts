b0cc73dd0823b91912762cd48f0638557c730b86
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-07-12 16:11:15 -0300

    created repository to answers

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswerRepository } from "../repositories/answer-repository";
+import { Answer } from "../entities/answer";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswerRepository: AnswerRepository = {
+  create: async function (answer: Answer): Promise<void> {
+    return;
+  },
+};
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+  const answer = await answerQuestion.execute({
     instructorId: "1",
     questionId: "2",
     content: "essa é a resposta",
