755064210100d642114a68b65efb16e73b99db78
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-07-13 16:57:11 -0300

    created slug value-object

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("it shoould be able to create a new slug from text", () => {
+  const slug = Slug.createFromText("Example Text");
+
+  expect(slug.value).toEqual("example-text");
+});
