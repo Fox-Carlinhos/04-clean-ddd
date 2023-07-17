b898a2065f1a70102f72730210a214d8596dfcf5
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-07-17 12:43:08 -0300

    chore:refact archives

---
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -0,0 +1,19 @@
+import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswerRepository } from "../repositories/answer-repository";
+import { Answer } from "../../enterprise/entities/answer";
+
+const fakeAnswerRepository: AnswerRepository = {
+  create: async function (answer: Answer): Promise<void> {},
+};
+
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+  const answer = await answerQuestion.execute({
+    instructorId: "1",
+    questionId: "2",
+    content: "essa é a resposta",
+  });
+
+  expect(answer.content).toEqual("essa é a resposta");
+});
