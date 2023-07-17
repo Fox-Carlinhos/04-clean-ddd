8e9bf7f3aa510841ff28367cbd65172e1bd5ae2f
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-07-17 12:24:12 -0300

    eslint config

--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -1,8 +1,8 @@
-import { expect, test } from "vitest";
-import { Slug } from "./slug";
+import { expect, test } from 'vitest'
+import { Slug } from './slug'
 
-test("it shoould be able to create a new slug from text", () => {
-  const slug = Slug.createFromText("Example Text");
+test('it shoould be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Example Text')
 
-  expect(slug.value).toEqual("example-text");
-});
+  expect(slug.value).toEqual('example-text')
+})
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,21 +1,19 @@
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswerRepository } from "../repositories/answer-repository";
-import { Answer } from "../entities/answer";
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answer-repository'
+import { Answer } from '../entities/answer'
 
 const fakeAnswerRepository: AnswerRepository = {
-  create: async function (answer: Answer): Promise<void> {
-    return;
-  },
-};
+  create: async function (answer: Answer): Promise<void> {},
+}
 
-test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
   const answer = await answerQuestion.execute({
-    instructorId: "1",
-    questionId: "2",
-    content: "essa é a resposta",
-  });
+    instructorId: '1',
+    questionId: '2',
+    content: 'essa é a resposta',
+  })
 
-  expect(answer.content).toEqual("essa é a resposta");
-});
+  expect(answer.content).toEqual('essa é a resposta')
+})
